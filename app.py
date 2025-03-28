34f04542a5868153906581efae41b5446b5eb8ad
SaulQuadros <126301913+SaulQuadros@users.noreply.github.com>
2025-03-28 13:46:51 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,7 @@
 # In[ ]:
 
 
-Vamos fazer o seguinte, ao invés de usar páginas HPML, quando o usuário clicar na opção "Sobre o Modelo import streamlit as st
+#Vamos fazer o seguinte, ao invés de usar páginas HPML, quando o usuário clicar na opção "Sobre o Modelo import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
